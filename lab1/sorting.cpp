9a71ce5f4ea40dbc6c02028bf131f928aefd014a
leftover <113136731+leftover19@users.noreply.github.com>
2022-10-02 11:48:22 +0530

    Add bubble sort
    Co-authored-by: Saksham Mittal <gotlougit@users.noreply.github.com>

--- lab1/sorting.cpp
+++ lab1/sorting.cpp
@@ -27,6 +27,16 @@ void selectionSort(vector<int> &arr) {
 	}
 }
 
+void bubbleSort(vector<int> &arr) {
+	for (int i = 0; i < n - 1; i++) {
+        	for (int j = 0; j < n - i - 1; j++) {
+            		if (arr[j] > arr[j + 1]) {
+                		swap(arr[j], arr[j + 1]);
+			}
+		}
+	}
+}
+
 void printArr(vector<int> &arr) {
 	for (auto num: arr) {
 		cout << num << " ";
