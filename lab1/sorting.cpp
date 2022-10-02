92a9e31b93079284987bade3a462543d91db1482
Saksham Mittal <gotlougit@users.noreply.github.com>
2022-10-02 06:10:05 +0000

    Fix formatting issues in bubbleSort()

--- lab1/sorting.cpp
+++ lab1/sorting.cpp
@@ -27,12 +27,12 @@ void selectionSort(vector<int> &arr) {
 	}
 }
 
-void bubbleSort(vector<int> &arr)
-{
-	for (int i = 0; i < n - 1; i++){
-        	for (int j = 0; j < n - i - 1; j++){
-            		if (arr[j] > arr[j + 1])
-                	swap(arr[j], arr[j + 1]);
+void bubbleSort(vector<int> &arr) {
+	for (int i = 0; i < n - 1; i++) {
+        	for (int j = 0; j < n - i - 1; j++) {
+            		if (arr[j] > arr[j + 1]) {
+                		swap(arr[j], arr[j + 1]);
+			}
 		}
 	}
 }
